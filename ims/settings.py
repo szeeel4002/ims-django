fb42b46447385669fe92500edacdc383ae77cb23
zeelsheth006 <zeelsheth1@gmail.com>
2025-11-19 10:55:22 +0530

    Initial Django project setup

--- ims/settings.py
+++ ims/settings.py
@@ -72,7 +72,8 @@ ROOT_URLCONF = 'ims.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
